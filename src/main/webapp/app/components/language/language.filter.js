aeebe1e45ec3a47e58c1c5f5aa8017d71b8c595b
Ederagp <eder.gomezp@gmail.com>
2017-08-28 18:31:21 -0500

    Added language.

--- src/main/webapp/app/components/language/language.filter.js
+++ src/main/webapp/app/components/language/language.filter.js
@@ -7,6 +7,7 @@
         .filter('findLanguageRtlFromKey', findLanguageRtlFromKey);
 
     var languages = {
+        'es': { name: 'Español' }
         // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
     };
 
